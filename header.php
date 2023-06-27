4510a0c6dff968c8b948e331172e8c30cd403e56
바가나 <86461360+whynot1217@users.noreply.github.com>
2023-06-27 13:06:03 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,18 +1,4 @@
-<?
-	include "/home/mokyang/www/module/login/head.php";	
-	include "/home/mokyang/www/module/class/class.DbCon.php";
-	include "/home/mokyang/www/module/class/class.Util.php";
-	include "/home/mokyang/www/module/class/class.Msg.php";
-	require_once '/home/mokyang/www/module/Mobile-Detect-master/Mobile_Detect.php';
-	$detect = new Mobile_Detect;
 
-	if($detect->isiOS())						$UserDevice = "ios";
-	elseif($detect->isAndroidOS())		$UserDevice = "android";
-	else										$UserDevice = "";
-	
-	if($detect->isMobile())	$UserOS = 'mobile';
-	else							$UserOS = 'pc';
-?>
 
 
 <style>
